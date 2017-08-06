001883903c02ede9261da7b822806b171b7dcdb1
Samuel Čavoj <sammko@sammserver.com>
2017-08-06 12:04:52 +0200

    Replace tabs with spaces and handle device deletion events. (Fix #1)

--- airplaneledd.c
+++ airplaneledd.c
@@ -12,44 +12,47 @@
 #define NDEVS 64
 
 int main() {
-	int ret = 0;
-	int afd = open("/sys/class/leds/" LEDNAME "/trigger", O_WRONLY);
-	if (afd == -1) return errno;
-	write(afd, "none", 4);
-	close(afd);
+    int ret = 0;
+    int afd = open("/sys/class/leds/" LEDNAME "/trigger", O_WRONLY);
+    if (afd == -1) return errno;
+    write(afd, "none", 4);
+    close(afd);
 
-	int rf_fd = open("/dev/rfkill", O_RDONLY);
-	struct rfkill_event ev;
-	size_t devcount = 0;
-	size_t bcount = 0;
-	int states[NDEVS];
-	memset(states, 0, NDEVS*sizeof(int));
+    int rf_fd = open("/dev/rfkill", O_RDONLY);
+    struct rfkill_event ev;
+    size_t devcount = 0;
+    size_t bcount = 0;
+    int states[NDEVS];
+    memset(states, 0, NDEVS*sizeof(int));
 
-	while (read(rf_fd, (void*)&ev, sizeof(ev)) == RFKILL_EVENT_SIZE_V1) {
-		if (ev.idx >= NDEVS) continue;
-		if (ev.op == RFKILL_OP_ADD) {
-			++devcount;
-			bcount += ev.hard || ev.soft;
-		} else {
-			if (ev.hard || ev.soft) {
-				if (!states[ev.idx]) {
-					states[ev.idx] = 1;
-					bcount++;
-				}
-			} else {
-				bcount--;
-				states[ev.idx] = 0;
-			}
-		}
-		int afd = open("/sys/class/leds/" LEDNAME "/brightness", O_WRONLY);
-		if (afd == -1) {
-			ret = errno;
-			goto end;
-		}
-		write(afd, (bcount == devcount)?"1":"0", 1);
-		close(afd);
-	}
+    while (read(rf_fd, (void*)&ev, sizeof(ev)) == RFKILL_EVENT_SIZE_V1) {
+        if (ev.idx >= NDEVS) continue;
+        if (ev.op == RFKILL_OP_ADD) {
+            ++devcount;
+            bcount += ev.hard || ev.soft;
+        } else if (ev.op == RFKILL_OP_DEL) {
+            --devcount;
+            bcount -= states[ev.idx];
+        } else {
+            if (ev.hard || ev.soft) {
+                if (!states[ev.idx]) {
+                    states[ev.idx] = 1;
+                    bcount++;
+                }
+            } else {
+                bcount--;
+                states[ev.idx] = 0;
+            }
+        }
+        int afd = open("/sys/class/leds/" LEDNAME "/brightness", O_WRONLY);
+        if (afd == -1) {
+            ret = errno;
+            goto end;
+        }
+        write(afd, (bcount == devcount)?"1":"0", 1);
+        close(afd);
+    }
 end:
-	close(rf_fd);
-	return ret;
+    close(rf_fd);
+    return ret;
 }
